342c5e4152cca5430041340c2c9e6678933ee024
kedi <kedi@kedi.kedi>
2016-03-01 05:54:37 -0500

    changes in the files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,53 +19,16 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
+      
       .when('/', {
-        templateUrl: 'views/driver_logo.html'
-      })
-      .when('/sign_in', {
         templateUrl: 'views/sign_in.html',
         controller: 'Sign_inCtrl'
       })
-      .when('/dashboard', {
-        resolve : {
-           "check" : function($location, $rootScope) {
-                    if(!$rootScope.loggedIn) {
-                       $location.path('/');
-                    }
-           }
-        },
-        templateUrl: 'views/dashboard.html',
-        controller: 'dashboardCtrl'
+      .when('/licensePlate', {
+        templateUrl: 'views/driver/licensePlate.html',
+        
       })
-      .when('/dashboardCard', {
-        resolve : {
-           "check" : function($location, $rootScope) {
-                    if(!$rootScope.loggedIn) {
-                       $location.path('/');
-                    }
-           }
-        },
-        templateUrl: 'views/dashboardCard.html',
-        controller: ''
-      })
-      .when('/deliveryAssociate', {
-        templateUrl: 'views/delivery_associate.html'
-      })
-      .when('/checkIn', {
-        templateUrl: 'views/check_in.html',
-        controller: 'checkInCtrl'
-      })
-      .when('/listedDrivers', {
-        resolve : {
-           "check" : function($location, $rootScope) {
-                    if(!$rootScope.loggedIn) {
-                       $location.path('/');
-                    }
-           }
-        },
-        templateUrl: 'views/listed_drivers.html'
-       }) 
-      .otherwise({
+       .otherwise({
         redirectTo: '/'
       });
   });
